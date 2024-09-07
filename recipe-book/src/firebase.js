c2f68a23a3be28388f631986db80953d88b9ba25
Mihaly Lovrencsics <lomysa@gmail.com>
2024-09-07 19:06:20 +0000

    Add firebase configuration to react app

---
+++ recipe-book/src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqphF9yWA1QnGUiXiZpnijIz11NSqNGQQ",
+  authDomain: "recipe-book-app-92ee1.firebaseapp.com",
+  projectId: "recipe-book-app-92ee1",
+  storageBucket: "recipe-book-app-92ee1.appspot.com",
+  messagingSenderId: "673590837230",
+  appId: "1:673590837230:web:bf2010b71eae613e7918ff",
+  measurementId: "G-SB6LCZ0HDD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
