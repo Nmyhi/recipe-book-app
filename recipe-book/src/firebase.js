654a2b6411e93f56af6e66155ba513d392f9d75c
Mihaly Lovrencsics <lomysa@gmail.com>
2024-10-07 14:11:22 +0000

    Set up for developement

--- recipe-book/src/firebase.js
+++ recipe-book/src/firebase.js
@@ -1,25 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqphF9yWA1QnGUiXiZpnijIz11NSqNGQQ",
-  authDomain: "recipe-book-app-92ee1.firebaseapp.com",
-  projectId: "recipe-book-app-92ee1",
-  storageBucket: "recipe-book-app-92ee1.appspot.com",
-  messagingSenderId: "673590837230",
-  appId: "1:673590837230:web:bf2010b71eae613e7918ff",
-  measurementId: "G-SB6LCZ0HDD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
